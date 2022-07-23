39edbfa4a20db48127ee445459d3b6e29da2a6ec
Enrique Solis <esolis205@yahoo.com>
2022-07-22 19:07:24 -0700

    Added ignoreBuildErrors to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: [ "www.linkedin.com", "m.media-amazon.com", "lh3.googleusercontent.com" ]
